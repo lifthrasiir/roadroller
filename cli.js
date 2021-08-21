2d16649bc3e75db0fdf3827a0dcebcb610ae230b
Kang Seonghoon <public+git@mearie.org>
2021-08-21 19:54:41 +0900

    Reorganize cli.js to functions.

--- cli.js
+++ cli.js
@@ -76,158 +76,180 @@ function readToString(stream) {
     })
 }
 
-const inputs = [];
-let currentInput = {};
-const options = {
-    arrayBufferPool: new ArrayBufferPool(),
-};
-let optimize;
-let outputPath;
-let nextIsArg = false;
-let silent = false;
+async function parseArgs(args) {
+    const inputs = [];
+    let currentInput = {};
+    const options = {
+        arrayBufferPool: new ArrayBufferPool(),
+    };
+    let optimize;
+    let outputPath;
+    let nextIsArg = false;
+    let silent = false;
 
-for (let i = 2; i < process.argv.length; ++i) {
-    const opt = process.argv[i];
-    if (nextIsArg || opt === '-' || !opt.startsWith('-')) {
-        nextIsArg = false;
-        try {
-            if (opt === '-') {
-                currentInput.data = await readToString(process.stdin);
-            } else {
-                currentInput.data = fs.readFileSync(opt, { encoding: 'utf-8' });
+    for (let i = 0; i < args.length; ++i) {
+        const opt = args[i];
+        if (nextIsArg || opt === '-' || !opt.startsWith('-')) {
+            nextIsArg = false;
+            try {
+                if (opt === '-') {
+                    currentInput.data = await readToString(process.stdin);
+                } else {
+                    currentInput.data = fs.readFileSync(opt, { encoding: 'utf-8' });
+                }
+            } catch (e) {
+                throw `cannot read from the input path ${opt}`;
             }
-        } catch (e) {
-            throw `cannot read from the input path ${opt}`;
-        }
-        if (currentInput.type === undefined) {
-            if (opt.endsWith('.js')) {
-                currentInput.type = 'js';
-            } else {
-                throw `cannot infer the input type for ${opt}`;
+            if (currentInput.type === undefined) {
+                if (opt.endsWith('.js')) {
+                    currentInput.type = 'js';
+                } else {
+                    throw `cannot infer the input type for ${opt}`;
+                }
             }
-        }
-        if (currentInput.action === undefined) {
-            if (currentInput.type === 'js') {
-                currentInput.action = 'eval';
-            } else {
-                throw `cannot infer the input action for ${opt}`;
+            if (currentInput.action === undefined) {
+                if (currentInput.type === 'js') {
+                    currentInput.action = 'eval';
+                } else {
+                    throw `cannot infer the input action for ${opt}`;
+                }
             }
+            inputs.push(currentInput);
+            currentInput = {};
+            continue;
         }
-        inputs.push(currentInput);
-        currentInput = {};
-        continue;
+
+        const getArg = m => {
+            if (m[1]) return m[1];
+            if (++i >= args.length) throw `invalid ${opt} argument`;
+            return args[i];
+        };
+        let m;
+        if (opt.match(/^(?:-h|--help)$/)) {
+            return { command: 'usage' };
+        } else if (opt.match(/^(?:-V|--version)$/)) {
+            return { command: 'version' };
+        } else if (opt.match(/^(?:-q|--silent)$/)) {
+            silent = true;
+        } else if (m = opt.match(/^(?:-t|--type)=?(.*)$/)) {
+            if (currentInput.type !== undefined) throw 'duplicate --type arguments';
+            currentInput.type = getArg(m);
+        } else if (m = opt.match(/^(?:-a|--action)=?(.*)$/)) {
+            if (currentInput.action !== undefined) throw 'duplicate --action arguments';
+            currentInput.action = getArg(m);
+        } else if (m = opt.match(/^(?:-o|--output-file)=?(.*)$/)) {
+            if (outputPath !== undefined) throw 'duplicate --output-file arguments';
+            outputPath = getArg(m);
+        } else if (m = opt.match(/^(?:-O|--optimize)=?(.*)$/)) {
+            if (optimize !== undefined) throw 'duplicate --optimize arguments';
+            optimize = Number(getArg(m));
+            if (optimize !== 0 && optimize !== 1) throw 'invalid --optimize argument';
+        } else if (m = opt.match(/^(?:-M|--max-memory)=?(.*)$/)) {
+            if (options.maxMemoryMB !== undefined) throw 'duplicate --max-memory arguments';
+            options.maxMemoryMB = parseInt(getArg(m), 10);
+            if (!(10 <= options.maxMemoryMB && options.maxMemoryMB <= 1024)) throw 'invalid --max-memory argument';
+        } else if (m = opt.match(/^(?:-S|--selectors)=?(.*)$/)) {
+            if (options.sparseSelectors !== undefined) throw 'duplicate --max-memory arguments';
+            const arg = getArg(m);
+            let selectors;
+            if (arg.match(/^x[0-9]+$/)) {
+                const numContexts = parseInt(arg.substr(1), 10);
+                if (numContexts > 64) throw 'too many selectors in --selectors argument';
+                selectors = defaultSparseSelectors(numContexts);
+            } else {
+                selectors = arg.split(/,/g).map(v => parseInt(v, 10)).sort((a, b) => a - b);
+                if (selectors.length > 64) throw 'too many selectors in --selectors argument';
+                if (selectors.some(sel => sel !== sel || sel < 0)) throw 'invalid selector in --selectors argument';
+                if (selectors.find((sel, i) => selectors[i-1] === sel)) throw 'duplicate selector in --selectors argument';
+            }
+            options.sparseSelectors = selectors;
+        } else if (opt == '--') {
+            nextIsArg = true;
+        } else {
+            throw `unknown option ${opt}`;
+        }
     }
 
-    const getArg = m => {
-        if (m[1]) return m[1];
-        if (++i >= process.argv.length) throw `invalid ${opt} argument`;
-        return process.argv[i];
-    };
-    let m;
-    if (opt.match(/^(?:-h|--help)$/)) {
+    if (Object.keys(currentInput).length > 0) {
+        throw 'the last input path is missing';
+    }
+
+    if (inputs.length === 0) {
+        return { command: 'default' };
+    }
+    if (optimize === undefined) optimize = 0;
+    if (outputPath === undefined) outputPath = '-';
+    return { command: 'compress', inputs, options, optimize, outputPath, silent };
+}
+
+async function compress({ inputs, options, optimize, outputPath, silent }) {
+    let packer = new Packer(inputs, options);
+
+    if (optimize) {
+        // the js input can be freely changed to the text, see if it fares better
+        let preferText = false;
+        if (inputs[0].type === 'js') {
+            const origSize = packer.makeDecoder().estimateLength();
+            inputs[0].type = 'text';
+            const textPacker = new Packer(inputs, options);
+            const textSize = textPacker.makeDecoder().estimateLength();
+            inputs[0].type = 'js';
+
+            if (textSize < origSize) {
+                if (!silent) console.warn(`switch the JS input to the text:`, textSize);
+                packer = textPacker;
+                preferText = true;
+            }
+        }
+
+        const result = await packer.optimizeSparseSelectors(info => {
+            if (silent) return;
+            console.warn(
+                (info.temperature > 1 ? '(baseline)' : `(T=${info.temperature.toFixed(4)})`) +
+                    ` trying ${JSON.stringify(info.current)}:`,
+                info.currentSize, info.bestUpdated ? '<-' : info.currentRejected ? 'x' : '');
+        });
+        if (!silent) {
+            console.warn(
+                `search done in ${(result.elapsedMsecs / 1000).toFixed(1)}s, ` +
+                `use \`${preferText ? '-t text ' : ''}-S ${result.best.join(',')}\` to replicate:`,
+                result.bestSize);
+        }
+    }
+
+    const packed = packer.makeDecoder();
+    const output = packed.firstLine + '\n' + packed.secondLine;
+    const origLength = inputs.reduce((acc, { data } ) => {
+        return acc + (Array.isArray(data) ? data.length : unescape(encodeURIComponent(data)).length);
+    }, 0);
+    const compressedLength = packed.estimateLength();
+    const ratio = origLength > 0 ? 100 - compressedLength / origLength * 100 : -Infinity;
+    if (!optimize && !silent) {
+        console.warn(`compressed ${origLength}B into ${compressedLength}B (estimated, ${Math.abs(ratio).toFixed(2)}% ${ratio > 0 ? 'smaller' : 'larger'}).`);
+    }
+    if (outputPath === '-') {
+        console.log(output); // this includes a trailing newline, use the file output to get rid of it
+    } else {
+        fs.writeFileSync(outputPath, output, { encoding: 'utf-8' });
+    }
+}
+
+const parsed = await parseArgs(process.argv.slice(2));
+switch (parsed.command) {
+    case 'compress':
+        await compress(parsed);
+        break;
+
+    case 'usage':
         usage();
         process.exit(0);
-    } else if (opt.match(/^(?:-V|--version)$/)) {
+
+    case 'version':
         console.log(`Roadroller ${VERSION}`);
         process.exit(0);
-    } else if (opt.match(/^(?:-q|--silent)$/)) {
-        silent = true;
-    } else if (m = opt.match(/^(?:-t|--type)=?(.*)$/)) {
-        if (currentInput.type !== undefined) throw 'duplicate --type arguments';
-        currentInput.type = getArg(m);
-    } else if (m = opt.match(/^(?:-a|--action)=?(.*)$/)) {
-        if (currentInput.action !== undefined) throw 'duplicate --action arguments';
-        currentInput.action = getArg(m);
-    } else if (m = opt.match(/^(?:-o|--output-file)=?(.*)$/)) {
-        if (outputPath !== undefined) throw 'duplicate --output-file arguments';
-        outputPath = getArg(m);
-    } else if (m = opt.match(/^(?:-O|--optimize)=?(.*)$/)) {
-        if (optimize !== undefined) throw 'duplicate --optimize arguments';
-        optimize = Number(getArg(m));
-        if (optimize !== 0 && optimize !== 1) throw 'invalid --optimize argument';
-    } else if (m = opt.match(/^(?:-M|--max-memory)=?(.*)$/)) {
-        if (options.maxMemoryMB !== undefined) throw 'duplicate --max-memory arguments';
-        options.maxMemoryMB = parseInt(getArg(m), 10);
-        if (!(10 <= options.maxMemoryMB && options.maxMemoryMB <= 1024)) throw 'invalid --max-memory argument';
-    } else if (m = opt.match(/^(?:-S|--selectors)=?(.*)$/)) {
-        if (options.sparseSelectors !== undefined) throw 'duplicate --max-memory arguments';
-        const arg = getArg(m);
-        let selectors;
-        if (arg.match(/^x[0-9]+$/)) {
-            const numContexts = parseInt(arg.substr(1), 10);
-            if (numContexts > 64) throw 'too many selectors in --selectors argument';
-            selectors = defaultSparseSelectors(numContexts);
-        } else {
-            selectors = arg.split(/,/g).map(v => parseInt(v, 10)).sort((a, b) => a - b);
-            if (selectors.length > 64) throw 'too many selectors in --selectors argument';
-            if (selectors.some(sel => sel !== sel || sel < 0)) throw 'invalid selector in --selectors argument';
-            if (selectors.find((sel, i) => selectors[i-1] === sel)) throw 'duplicate selector in --selectors argument';
-        }
-        options.sparseSelectors = selectors;
-    } else if (opt == '--') {
-        nextIsArg = true;
-    } else {
-        throw `unknown option ${opt}`;
-    }
-}
 
-if (Object.keys(currentInput).length > 0) {
-    throw 'the last input path is missing';
-}
-if (inputs.length === 0) {
-    usage();
-    process.exit(1);
-}
-if (optimize === undefined) optimize = 0;
-if (outputPath === undefined) outputPath = '-';
-
-let packer = new Packer(inputs, options);
-
-if (optimize) {
-    // the js input can be freely changed to the text, see if it fares better
-    let preferText = false;
-    if (inputs[0].type === 'js') {
-        const origSize = packer.makeDecoder().estimateLength();
-        inputs[0].type = 'text';
-        const textPacker = new Packer(inputs, options);
-        const textSize = textPacker.makeDecoder().estimateLength();
-        inputs[0].type = 'js';
-
-        if (textSize < origSize) {
-            if (!silent) console.warn(`switch the JS input to the text:`, textSize);
-            packer = textPacker;
-            preferText = true;
-        }
-    }
-
-    const result = await packer.optimizeSparseSelectors(info => {
-        if (silent) return;
-        console.warn(
-            (info.temperature > 1 ? '(baseline)' : `(T=${info.temperature.toFixed(4)})`) +
-                ` trying ${JSON.stringify(info.current)}:`,
-            info.currentSize, info.bestUpdated ? '<-' : info.currentRejected ? 'x' : '');
-    });
-    if (!silent) {
-        console.warn(
-            `search done in ${(result.elapsedMsecs / 1000).toFixed(1)}s, ` +
-            `use \`${preferText ? '-t text ' : ''}-S ${result.best.join(',')}\` to replicate:`,
-            result.bestSize);
-    }
-}
-
-const packed = packer.makeDecoder();
-const output = packed.firstLine + '\n' + packed.secondLine;
-const origLength = inputs.reduce((acc, { data } ) => {
-    return acc + (Array.isArray(data) ? data.length : unescape(encodeURIComponent(data)).length);
-}, 0);
-const compressedLength = packed.estimateLength();
-const ratio = origLength > 0 ? 100 - compressedLength / origLength * 100 : -Infinity;
-if (!optimize && !silent) {
-    console.warn(`compressed ${origLength}B into ${compressedLength}B (estimated, ${Math.abs(ratio).toFixed(2)}% ${ratio > 0 ? 'smaller' : 'larger'}).`);
-}
-if (outputPath === '-') {
-    console.log(output); // this includes a trailing newline, use the file output to get rid of it
-} else {
-    fs.writeFileSync(outputPath, output, { encoding: 'utf-8' });
+    default:
+        usage();
+        process.exit(1);
 }
 
